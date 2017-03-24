77d1d2ee78cf8a96ea54a78c7022158692e1cd18
Enathen <Jontekaminen@hotmail.com>
2017-03-24 12:06:52 +0100

    Swagboy mc swag

--- Clock/src/Clock.java
+++ Clock/src/Clock.java
@@ -1,25 +1,69 @@
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by Enathen on 2017-03-23.
  */
 public class Clock {
-    String displayString = new String;
+    private String displayString = new String();
+    NumberDisplay hour = new NumberDisplay(0, 24);
+    NumberDisplay minutes = new NumberDisplay(0, 60);
 
     public Clock(){
 
-    }
-    public void clockCreate(int hour, int minute){
+        getTime();
+        System.out.println(displayString);
+
+            long time = System.currentTimeMillis();
+            Runnable update = new Runnable(){
+                public void run(){
+                    timeTick();
+                }
+            };
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask(){
+                public void run(){
+                    update.run();
+                }
+            }, time % 10000, 10000);
+        //update.run();
+
+
+
+
 
     }
-    public void timeTick(){
+    private void Clock(int hour, int minute){
+
 
     }
-    public void setTime(int hour, int minute){
+    private void timeTick(){
+
+        boolean open = minutes.increment();
+        if(minutes.didWrapAround(open)) {
+            hour.increment();
+        }
+        getTime();
+        System.out.println(displayString );
+        if(hour.didWrapAround(open) && hour.getValue() == 0){
+            System.out.println("Good new day");
+        }
+
 
     }
-    public String getTime(){
+    private void setTime(int hour, int minute){
 
     }
-    public void updateDisplay(){
+    private void getTime(){
+        hour.getValue();
+        minutes.getValue();
+        displayString = new String();
+        displayString = displayString.concat(hour.getDisplayValue());
+        displayString = displayString.concat(minutes.getDisplayValue());
+
+    }
+    private void updateDisplay(){
 
     }
 }
